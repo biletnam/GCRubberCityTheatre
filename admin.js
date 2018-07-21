19d3743d8caf0a5f0ecf863fa1c3ade6d8857130
Phillip Dodson <clevergadget@gmail.com>
2018-07-21 15:29:16 -0400

    commit for merge

--- admin.js
+++ admin.js
@@ -21,43 +21,86 @@ jQuery(function(){
 			return false;
 		});
 	};
-	var showMessageView = function(){
-		_appMain.html(
-			'<ul class="messagesList"></ul>'
-			+ '<form class="messageForm">'
-				+ '<input class="messageField" name="message" autocomplete="off" autofocus="autofocus" required="required" /><button>Send</button>'
-			+ '</form>'
+	var showMessageView = function () {
+		_app.html(`
+			<nav class="navbar navbar-expand-lg navbar-light bg-light">
+				<a class="navbar-brand">Rubber City Theatre</a>
+			</nav>
+			<div class="messagesList"></div>
+			<form class="messageForm fixed-bottom">
+				<div class="input-group">
+					<input type="text" class="form-control messageField" name="message" autocomplete="off" autofocus="autofocus" required="required" placeholder="Enter Message" aria-label="Enter message..." aria-describedby="button-addon2">
+					<div class="input-group-append">
+						<button class="btn btn-outline-secondary" type="submit" id="button-addon2">Send</button>
+					</div>
+				</div>
+			</form>`
 		);
 		var _formEl = _app.find('.messageForm');
 		var _messageEl = _formEl.find('.messageField');
 		_messageListEl = _app.find('.messagesList');
-		_formEl.on('submit', function(){
+		_formEl.on('submit', function () {
 			var _message = _messageEl.val();
-			if(_message){
+			if (_message) {
 				_socket.emit('message', _message);
 				_messageEl.val('');
 			}
 			return false;
 		});
-	};
+	}
+
+	//--routing
+	showLoginView();
+	_socket.on('message', function (_message) {
+		console.log('message: ' + _message);
+
+		if (_message.type === 'admin') {
+			_messageListEl.append(jQuery(`
+			<div class="card">
+				<div class="card-body" >
+					<h6 class="card-title admin-header">
+						${_message.name} <span style="font-size:.7em;"> posted at </span> ${_message.time}
+					</h6>
+					<div style="display: flex; flex-direction: row; align-items:center; width:100%">
+						<div class="user profile-pic">
+						</div>
+						<div>
+							${_message.value}
+						</div>
+					</div>
+				</div>
+			</div>
+		`,
+				{ 'data-type': _message.type }
+			));
+		}
+		else if (_message.type === "user") {
+			_messageListEl.append(jQuery(`
+			<div class="card">
+				<div class="card-body">
+					<h6 class="card-title user-header">
+						${_message.name} <span style="font-size:.8em; margin-left:8px; margin-right:8px;">posted at</span> ${_message.time}
+					</h6>
+					<div style="display: flex; flex-direction: row; align-items:center; justify-content:flex-end;">
+
+						<div>
+							${_message.value}
+						</div>
+						<div class="user profile-pic">
+						</div>
+					</div>
+				</div>
+			</div>
+		`, ))
+		}
+	});
 
 	//--routing
 	showLoginView();
 	_socket.on('adminLoggedIn', function(){
 		showMessageView();
 	});
-	_socket.on('message', function(_message){
-		console.log('message: ' + _message);
-		var _liEl = jQuery('<li>', {'data-type': _message.type});
-		switch(_message.type){
-			case 'user':
-				_messageListEl.append(_liEl.text(_message.value));
-			break;
-			case 'admin':
-				_messageListEl.append(_liEl.text(_message.value));
-			break;
-		}
-	});
+
 	_socket.on('reconnect', function(){
 		showLoginView();
 	});
