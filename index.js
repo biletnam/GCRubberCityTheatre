cf88921cde3082aedbb303434ec9a01d415bdb42
Steven Beard <etacalpha@gmail.com>
2018-07-21 16:33:11 -0400

    date format updated

--- index.js
+++ index.js
@@ -62,7 +62,7 @@ _io.on('connection', function(_socket){
 			}
 			var _minutes = _now.getMinutes();
 			if(_minutes < 9){
-				_minutes = '0' + _minutes;
+				_minutes = '0' + _minutes.toString();
 			}
 			_time += ':' + _minutes + ' ' + _amPm;
 			console.log('time: ' + _time);
