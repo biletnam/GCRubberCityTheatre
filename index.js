56b1a4a4210772ee4a5ea4371dac35e5aa4ba1d1
Steven Beard <etacalpha@gmail.com>
2018-07-21 22:08:38 -0400

    fixed merge conflicts

--- index.js
+++ index.js
@@ -41,6 +41,29 @@ lines.forEach(function(line){
 			_adminUserNames=_holder[1].split(",").map(function(_name){ return _name.trim()});
 		break;
 	};
+    readline = require('readline');
+
+//--data
+
+var _adminUserNamesString = process.env['ADMIN_NAMES'] || 'Admin|Other Person';
+var _adminUserNames = _adminUserNamesString.split('|');
+var _approvedMessages = [];
+var _users = [];
+var _userMessages = [];
+//var _adminPassword = process.env['ADMIN_PASSWORD'] || '123456';
+var _adminUsers = [];
+var _userPassword = process.env['USER_PASSWORD'] || '1234';
+
+var rd = readline.createInterface({
+    input: fs.createReadStream(__dirname+'/app.setup'),
+    output: process.stdout,
+    console: false
+});
+rd.on('line', function(line) {
+	var holder = line.split(":");
+	if (holder[0].toLowerCase()=="password") {
+		_adminPassword = holder[1]; 
+	}
 });
 
 //--sockets
